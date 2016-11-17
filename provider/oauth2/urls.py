3660537d1a2f854a0c3a05dd10e90915c1421f68
diego kogan <dkogan@devartis.com>
2016-11-17 11:21:53 -0300

    Update to django 1.10
    drop django < 1.8 compatibility

--- provider/oauth2/urls.py
+++ provider/oauth2/urls.py
@@ -32,24 +32,15 @@ that are meant for client (as defined in :rfc:`1`) interaction.
     Errors are outlined in :rfc:`5.2`.
 
 """
-
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from ..compat.urls import *
+
 from .views import Authorize, Redirect, Capture, AccessTokenView
 
-
-urlpatterns = patterns('',
-    url('^authorize/?$',
-        login_required(Capture.as_view()),
-        name='capture'),
-    url('^authorize/confirm/?$',
-        login_required(Authorize.as_view()),
-        name='authorize'),
-    url('^redirect/?$',
-        login_required(Redirect.as_view()),
-        name='redirect'),
-    url('^access_token/?$',
-        csrf_exempt(AccessTokenView.as_view()),
-        name='access_token'),
-)
+urlpatterns = [
+    url('^authorize/?$', login_required(Capture.as_view()), name='capture'),
+    url('^authorize/confirm/?$', login_required(Authorize.as_view()), name='authorize'),
+    url('^redirect/?$', login_required(Redirect.as_view()), name='redirect'),
+    url('^access_token/?$', csrf_exempt(AccessTokenView.as_view()), name='access_token'),
+]
